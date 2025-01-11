f355305772e00264a7c40f7ddd9ed35a2e998abe
PhwgNamTran <phwgnam@gmail.com>
2025-01-11 22:06:53 +0700

    Add init and main function

---
+++ CM4/Headers/CM4_init.h
@@ -0,0 +1,14 @@
+#ifndef CM4_INIT_H
+#define CM4_INIT_H
+
+/*
+ * System Initialization:
+ * ----------------------
+ * This header file declares functions for initializing the system and peripherals.
+ */
+
+
+extern void System_Init(void);         /**< Initialize the system */
+
+
+#endif /* SYSTEM_INIT_H */
---
+++ CM4/Sources/CM4_init.c
@@ -0,0 +1,20 @@
+#include "CM4_init.h"
+/*
+ * Function: System_Init
+ * ---------------------
+ * Initializes the system.
+ *
+ * This function initializes various peripherals and configurations required
+ * for the system to operate.
+ *
+ * Input:
+ *   None
+ *
+ * Returns:
+ *   None
+ */
+void System_Init(void)
+{
+    // Implement initialization code here
+}
+
