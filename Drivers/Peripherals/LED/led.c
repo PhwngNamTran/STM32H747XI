bd995c09cd4d6b88448cca950e4a2181610eabbd
PhwgNamTran <phwgnam@gmail.com>
2025-01-14 21:40:44 +0700

    Update led lib

--- Drivers/Peripherals/LED/led.c
+++ Drivers/Peripherals/LED/led.c
@@ -6,39 +6,16 @@
  * Initializes LED:
  *   - Configures GPIO mode, pull resistor, and output type.
  *   - Sets default state to LED OFF.
+ *
+ * Parameters:
+ *   led: LED to initialize
  */
-void LED_Init(uint8_t LEDx)
+void LED_Init(LED_TypeDef led)
 {
-    switch (LEDx)
-    {
-        case LED_1:
-            GPIO_Mode_Set(LED_Port, LED_1_PIN, GPIO_Mode_General_Purpose_Output_N);
-            GPIO_Pull_Set(LED_Port, LED_1_PIN, GPIO_No_Pull_N);
-            GPIO_OutputType_Set(LED_Port, LED_1_PIN, GPIO_Output_PushPull_N);
-            LED_OFF(LED_1);
-            break;
-
-        case LED_2:
-            GPIO_Mode_Set(LED_Port, LED_2_PIN, GPIO_Mode_General_Purpose_Output_N);
-            GPIO_Pull_Set(LED_Port, LED_2_PIN, GPIO_No_Pull_N);
-            GPIO_OutputType_Set(LED_Port, LED_2_PIN, GPIO_Output_PushPull_N);
-            LED_OFF(LED_2);
-            break;
-
-        case LED_3:
-            GPIO_Mode_Set(LED_Port, LED_3_PIN, GPIO_Mode_General_Purpose_Output_N);
-            GPIO_Pull_Set(LED_Port, LED_3_PIN, GPIO_No_Pull_N);
-            GPIO_OutputType_Set(LED_Port, LED_3_PIN, GPIO_Output_PushPull_N);
-            LED_OFF(LED_3);
-            break;
-
-        case LED_4:
-            GPIO_Mode_Set(LED_Port, LED_4_PIN, GPIO_Mode_General_Purpose_Output_N);
-            GPIO_Pull_Set(LED_Port, LED_4_PIN, GPIO_No_Pull_N);
-            GPIO_OutputType_Set(LED_Port, LED_4_PIN, GPIO_Output_PushPull_N);
-            LED_OFF(LED_4);
-            break;
-    }
+    GPIO_Mode_Set(led.port, led.pin, GPIO_Mode_General_Purpose_Output_N);
+    GPIO_Pull_Set(led.port, led.pin, GPIO_No_Pull_N);
+    GPIO_OutputType_Set(led.port, led.pin, GPIO_Output_PushPull_N);
+    LED_OFF(led);
 }
 
 /*
@@ -47,31 +24,11 @@ void LED_Init(uint8_t LEDx)
  * Turns ON the specified LED.
  *
  * Parameters:
- *   LEDx: LED number to turn ON (LED_1, LED_2, LED_3, LED_4)
+ *   led: LED to turn ON
  */
-void LED_ON(uint8_t LEDx)
+void LED_ON(LED_TypeDef led)
 {
-    switch (LEDx)
-    {
-        case LED_1:
-            GPIO_OutputData_Reset(LED_Port, LED_1_PIN);
-            break;
-
-        case LED_2:
-            GPIO_OutputData_Reset(LED_Port, LED_2_PIN);
-            break;
-
-        case LED_3:
-            GPIO_OutputData_Reset(LED_Port, LED_3_PIN);
-            break;
-
-        case LED_4:
-            GPIO_OutputData_Reset(LED_Port, LED_4_PIN);
-            break;
-
-        default:
-            break;
-    }
+    GPIO_OutputData_Reset(led.port, led.pin);
 }
 
 /*
@@ -80,31 +37,11 @@ void LED_ON(uint8_t LEDx)
  * Turns OFF the specified LED.
  *
  * Parameters:
- *   LEDx: LED number to turn OFF (LED_1, LED_2, LED_3, LED_4)
+ *   led: LED to turn OFF
  */
-void LED_OFF(uint8_t LEDx)
+void LED_OFF(LED_TypeDef led)
 {
-    switch (LEDx)
-    {
-        case LED_1:
-            GPIO_OutputData_Set(LED_Port, LED_1_PIN);
-            break;
-
-        case LED_2:
-            GPIO_OutputData_Set(LED_Port, LED_2_PIN);
-            break;
-
-        case LED_3:
-            GPIO_OutputData_Set(LED_Port, LED_3_PIN);
-            break;
-
-        case LED_4:
-            GPIO_OutputData_Set(LED_Port, LED_4_PIN);
-            break;
-
-        default:
-            break;
-    }
+    GPIO_OutputData_Set(led.port, led.pin);
 }
 
 /*
@@ -113,29 +50,9 @@ void LED_OFF(uint8_t LEDx)
  * Toggles the state of the specified LED (ON to OFF or OFF to ON).
  *
  * Parameters:
- *   LEDx: LED number to toggle (LED_1, LED_2, LED_3, LED_4)
+ *   led: LED to toggle
  */
-void LED_TOGGLE(uint8_t LEDx)
+void LED_TOGGLE(LED_TypeDef led)
 {
-    switch (LEDx)
-    {
-        case LED_1:
-            GPIO_OutputData_Toggle(LED_Port, LED_1_PIN);
-            break;
-
-        case LED_2:
-            GPIO_OutputData_Toggle(LED_Port, LED_2_PIN);
-            break;
-
-        case LED_3:
-            GPIO_OutputData_Toggle(LED_Port, LED_3_PIN);
-            break;
-
-        case LED_4:
-            GPIO_OutputData_Toggle(LED_Port, LED_4_PIN);
-            break;
-
-        default:
-            break;
-    }
+    GPIO_OutputData_Toggle(led.port, led.pin);
 }
